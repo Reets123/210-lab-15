41d29a2b46226b884d9fabe3f02a62dd118ee39c
Reets123 <ibrahimalatig@gmail.com>
2024-09-26 05:42:14 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Lab 15: Movie Class
+// COMSC-210 - Ibrahim Alatig 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Movie {
+    pr
+}
